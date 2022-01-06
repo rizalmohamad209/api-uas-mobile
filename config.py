54f37f04d5b3f0d7ad928d55057e4718b734743f
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-01-07 03:57:18 +0700

    002. heroku postgres

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config(object):
     USERNAME = "root"
     PASSWORD = "Rizalmohamad123"
     JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
-    SQLALCHEMY_DATABASE_URI = 'mysql://'+USERNAME+':'+PASSWORD+'@'+HOST+'/'+DATABASE
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://keyhjbritoyqcj:804aadb24179eb9cbee788f80dea8929dd5d41030c271942fefb01f488d35ec4@ec2-35-173-83-57.compute-1.amazonaws.com:5432/dek1mr5jkf2kj9'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://'+USERNAME+':'+PASSWORD+'@'+HOST+'/'+DATABASE
+    SQLALCHEMY_DATABASE_URI = 'postgresql://piaaijqlullvfi:c7b5b771c4c5555ddbccff9c6b5e38147c2cf5db7451d7a5e7a3b3703a1aaef0@ec2-34-239-196-254.compute-1.amazonaws.com:5432/de7141salhmp9n'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
